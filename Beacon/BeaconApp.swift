d4b4a2fdaaa8e252579ff122ca2fcee8bee07a34
Rubins Dormeus <dormeusrubins@yahoo.com>
2026-06-25 12:34:56 -0400

    Initial Commit

---
+++ Beacon/BeaconApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeaconApp.swift
+//  Beacon
+//
+//  Created by Rubins Dormeus on 6/25/26.
+//
+
+import SwiftUI
+
+@main
+struct BeaconApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Beacon/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Beacon
+//
+//  Created by Rubins Dormeus on 6/25/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
